158ef5175d295dc18cfad4d2565fd01a2288512d
yauheni.prakapenka <>
2024-05-10 13:21:16 +0300

    Add dashboard ui

--- app/src/main/java/com/example/food_app/activity/MainActivity.java
+++ app/src/main/java/com/example/food_app/activity/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.food_app.activity;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.food_app.R;
 
